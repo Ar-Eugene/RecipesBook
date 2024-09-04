ce04c5310aa28aed8d17c5f4f4957727560dc029
dev_Ar <artemenkoe2711@gmail.com>
2024-09-04 19:57:26 +0400

    Сделал DrawerLayout, в нем, сделал смену  ImageView в зависимости от месяца установленого у пользователя на устройстве .Поменял иконку проекта. НУЖНО В MainActivity сделать читабельный код + доделать DrawerLayout чтобы была иконка на ToolBar

--- app/src/main/java/com/example/recipesbook/MainActivity.kt
+++ app/src/main/java/com/example/recipesbook/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.recipesbook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipesbook.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,13 +27,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val toolbar=binding.toolbar
+        setSupportActionBar(toolbar)
+
+        val navigationView: NavigationView = findViewById(R.id.naw_view)
+        // Получаем headerView из NavigationView
+        val headerView: View = navigationView.getHeaderView(0)
+        // Находим ImageView в headerView
+        val imageView: ImageView = headerView.findViewById(R.id.image_year)
+
+        // Получаем текущий месяц
+        val calendar = Calendar.getInstance()
+        val month = calendar.get(Calendar.MONTH)
+        // Выбираем изображение в зависимости от месяца
+        val imageResource = when (month) {
+            Calendar.DECEMBER,Calendar.JANUARY,Calendar.FEBRUARY, -> R.drawable.tangerines
+            Calendar.MARCH,Calendar.APRIL,Calendar.MAY -> R.drawable.error_image
+            Calendar.JUNE,Calendar.JULY,Calendar.AUGUST -> R.drawable.error_image
+            Calendar.SEPTEMBER,Calendar.OCTOBER,Calendar.NOVEMBER -> R.drawable.autumn_apples
+
+            else -> R.drawable.error_image
+        }
+
+        // Устанавливаем изображение в ImageView
+        imageView.setImageDrawable(ContextCompat.getDrawable(this, imageResource))
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         val bottomNavigationView = binding.bottomNavigation
+        val drawer=binding.drawerLayout
+        val builder=AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(drawer)
         //recyclerView = findViewById(R.id.recycler_view)
 //        recyclerView.layoutManager = LinearLayoutManager(this)
 //        recyclerView.adapter = CategoryAdapter(categoryItems)
 
         bottomNavigationView.setupWithNavController(navController)
+    }
+    fun onCreatedOptionsMenu(menu: Menu):Boolean{
+        menuInflater.inflate(R.menu.action_save,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
--- app/src/main/java/com/example/recipesbook/MenuFragment.kt
+++ app/src/main/java/com/example/recipesbook/MenuFragment.kt
@@ -3,6 +3,7 @@ package com.example.recipesbook
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 
@@ -11,6 +12,7 @@ class MenuFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_menu, container, false)
     }
+
 
     companion object {
 
